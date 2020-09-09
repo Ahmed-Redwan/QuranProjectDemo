390054ece9bca872ec1940c5738773d521c6ff70
Ahmed-Redwan <MINGW64>
2020-09-09 13:51:56 +0300

    update commit

---
+++ app/src/main/java/com/example/quranprojectdemo/RegisterAs.java
@@ -0,0 +1,14 @@
+package com.example.quranprojectdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterAs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_as);
+    }
+}
