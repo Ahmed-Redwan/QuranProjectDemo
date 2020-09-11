a9bbef70197c1bab2c95cd3569def74ad3f63953
Ahmed-Redwan <MINGW64>
2020-09-11 11:56:41 +0300

    update commit

---
+++ app/src/main/java/com/example/quranprojectdemo/AddNewStudent.java
@@ -0,0 +1,72 @@
+package com.example.quranprojectdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddNewStudent extends AppCompatActivity {
+    TextView tv_Add;
+    Button btn_Add,btn_Cancel;
+    EditText et_studentName, et_studentId, et_Phone, et_Email, et_Grade, et_Year, et_Month, et_Day;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_student);
+
+        tv_Add = findViewById(R.id.AddNewStudent_tv_AddStudent);
+        et_studentName = findViewById(R.id.AddNewStudent_et_StudentName);
+        et_studentId = findViewById(R.id.AddNewStudent_et_StudentId);
+        et_Email = findViewById(R.id.AddNewStudent_et_Email);
+        et_Phone = findViewById(R.id.AddNewStudent_et_PhoneNum);
+        et_Grade = findViewById(R.id.AddNewStudent_et_Grade);
+        et_Year = findViewById(R.id.AddNewStudent_et_year);
+        et_Month = findViewById(R.id.AddNewStudent_et_month);
+        et_Day = findViewById(R.id.AddNewStudent_et_day);
+        btn_Add = findViewById(R.id.AddNewStudent_btn_AddNewStudent);
+        btn_Cancel = findViewById(R.id.AddNewStudent_btn_cancel);
+
+
+        TextView_EditFont(tv_Add, "Hacen_Tunisia_Bold.ttf");
+        EditText_EditFont(et_studentName, "Hacen_Tunisia.ttf");
+        EditText_EditFont(et_studentId, "Hacen_Tunisia.ttf");
+        EditText_EditFont(et_Email, "Hacen_Tunisia.ttf");
+        EditText_EditFont(et_Grade, "Hacen_Tunisia.ttf");
+        EditText_EditFont(et_Phone, "Hacen_Tunisia.ttf");
+        EditText_EditFont(et_Month, "Hacen_Tunisia.ttf");
+        EditText_EditFont(et_Year, "Hacen_Tunisia.ttf");
+        EditText_EditFont(et_Day, "Hacen_Tunisia.ttf");
+
+        btn_Add.setTypeface(Typeface.createFromAsset(getAssets(), "Hacen_Tunisia.ttf"));
+        btn_Cancel.setTypeface(Typeface.createFromAsset(getAssets(), "Hacen_Tunisia.ttf"));
+        btn_Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btn_Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+
+    public void TextView_EditFont(TextView textView, String path) {
+        textView.setTypeface(Typeface.createFromAsset(getAssets(), path));
+    }
+
+    //change font type for edittext.
+    public void EditText_EditFont(EditText editText, String path) {
+        editText.setTypeface(Typeface.createFromAsset(getAssets(), path));
+    }
+
+}
